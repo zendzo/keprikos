519f9c69ae308d8a9dcdfa803aff37d66f0d8aed
zendzo <zendzzo@gmail.com>
2017-04-25 10:28:44 +0700

    return rented totalorder pending value to user.dashboard

--- app/Http/Controllers/User/PagesController.php
+++ app/Http/Controllers/User/PagesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Order;
 
 class PagesController extends Controller
 {
@@ -12,7 +13,19 @@ class PagesController extends Controller
     {
     	$dataKosts = Auth::user()->kosts;
 
-    	return view('user.pages.dashboard',compact('dataKosts'));
+    	$kostId = [];
+
+    	foreach ($dataKosts as $kost) {
+    		array_push($kostId, $kost['id']);
+    	}
+
+    	$totalOrdered = Order::whereIn('kost_id',$kostId)->count();
+
+    	$totalRented = Order::whereIn('kost_id',$kostId)->whereNotNull('paid_at')->count();
+
+    	$totalPending = Order::whereIn('kost_id',$kostId)->whereNull('paid_at')->count();
+
+    	return view('user.pages.dashboard',compact('dataKosts','totalOrdered','totalRented','totalPending'));
     }
 
     public function getBlank()
