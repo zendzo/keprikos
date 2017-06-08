0457da6e938fb8927dcd09afebb3876fdfdbfe0b
zendzo <zendzzo@gmail.com>
2017-06-08 14:56:52 +0700

    remove priceYearly priceDaily priceMonthly column

---
+++ database/migrations/2017_06_08_142629_NullableKostPrice.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NullableKostPrice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('kosts', function (Blueprint $table) {
+            $table->dropColumn(['priceDaily','priceWeekly','priceYearly']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('kosts', function (Blueprint $table) {
+            $table->string('priceDaily');
+            $table->string('priceWeekly');
+            $table->string('priceYearly');
+        });
+    }
+}
